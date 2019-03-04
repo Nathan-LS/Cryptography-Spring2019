3065e8d4cc79757220775033a7ec873a0ef747d5
jsprchn2 <39418859+jsprchn2@users.noreply.github.com>
2019-03-04 07:52:41 -0800

    Update PlayFair.java

--- src/main/java/Ciphers/PlayFair.java
+++ src/main/java/Ciphers/PlayFair.java
@@ -2,8 +2,46 @@ package Ciphers;
 
 public class PlayFair extends CipherAbstractBase {
 
+	@Override
+	public void setKey(String k)
+	{
+		key = k;
+	}
+	
+	@Override
+	public String [] Pairs (string plaintext) {
+		int size = plaintext.length();
+		
+		if(size % 2 != 0)
+			size++;
+		
+		String x[] = new String[size / 2];
+		
+		int counter = 0;
+		
+		for (int i = 0; i < size / 2; i++)
+		{
+			x[i] = plaintext.substring(counter, counter + 2);
+			System.out.println(x[i]);
+			counter += 2;
+		}
+	
+		return x;
+	}
+	
     @Override
     public String encrypt(final String plaintext) { //TODO
+		
+		String result = "";
+		
+		String [][] encmatrix = new String[5][5];
+		
+		for(int i = 0; i < plaintext.length(); i++)
+		{
+			
+		}
+		
+
         return null;
     }
 
